548ab02061e59811075983403d8e9cb699f75570
nspoletaev <nspoletaev@avito.ru>
2023-09-17 23:53:41 +0400

    получаем конфиг приложения из .env

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/golang-migrate/migrate/v4 v4.16.2
+	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.4
 )
--- internal/app/dogs_api/config/config.go
+++ internal/app/dogs_api/config/config.go
@@ -1,14 +1,41 @@
 package config
 
 import (
+	"fmt"
 	"github.com/TriplePi/dogs/internal/app/dogs_api/domains/dog"
 	"github.com/TriplePi/dogs/internal/app/dogs_api/domains/user"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
-func initDb(dbUrl string) *gorm.DB {
+var envPath = "configs/.env"
+
+type Environment struct {
+	db  DbConfig
+	app AppConfig
+}
+
+type DbConfig struct {
+	url      string
+	name     string
+	login    string
+	password string
+}
+
+type AppConfig struct {
+	host string
+	port string
+}
+
+func initDb(environment Environment) *gorm.DB {
+	dbUrl := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
+		environment.db.login, environment.db.password, environment.db.url, environment.db.name,
+	)
+
 	RunMigrations(dbUrl)
 
 	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
@@ -23,16 +50,32 @@ func initDb(dbUrl string) *gorm.DB {
 	return db
 }
 
-func initRouter(db *gorm.DB) {
+func initRouter(db *gorm.DB, environment Environment) {
 	router := gin.Default()
 	dog.InitDogHandlers(router, db)
 	user.InitUserHandlers(router, db)
-	_ = router.Run("localhost:8080")
+	_ = router.Run(environment.app.host + ":" + environment.app.port)
 }
 
 func Init() {
-	dbUrl := "postgres://sobaka:nikitos_sosaka@localhost:5432/sobaki?sslmode=disable"
+	err := godotenv.Load(envPath)
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	db := initDb(dbUrl)
-	initRouter(db)
+	var environment = Environment{
+		app: AppConfig{
+			host: os.Getenv("APP_HOST"),
+			port: os.Getenv("APP_PORT"),
+		},
+		db: DbConfig{
+			url:      os.Getenv("DB_URL"),
+			name:     os.Getenv("DB_NAME"),
+			login:    os.Getenv("DB_LOGIN"),
+			password: os.Getenv("DB_PASSWORD"),
+		},
+	}
+
+	db := initDb(environment)
+	initRouter(db, environment)
 }
